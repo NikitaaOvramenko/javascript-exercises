c12d8f423d28af1b5ba55bc97d334fa13c2c810c
Nikita <ovramenko.nikitka@gmail.com>
2024-07-12 18:18:32 -0400

    finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,41 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
 
+    let sum = 0;
+
+    if((start < 0) || (end < 0)){
+        return "ERROR";
+    }
+
+    if(isNumber(start) == false || isNumber(end) == false){
+        return "ERROR";
+    }
+    
+
+     if(start < end){
+        
+        for(let i = start; i <= end; i++){
+            sum += i;
+        }
+     }
+
+     else if(start > end){
+        
+        for(let i = end; i <= start; i++){
+            sum += i;
+        }
+     }
+    
+    
+
+    
+    
+
+    return sum;
 };
+
+function isNumber(value) {
+    return typeof value === 'number';
+  }
 
 // Do not edit below this line
 module.exports = sumAll;
