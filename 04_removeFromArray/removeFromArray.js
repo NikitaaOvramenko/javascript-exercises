4a9c0ca4f98c21489bc30382c1bf8a89199ba476
Nikita <ovramenko.nikitka@gmail.com>
2024-07-12 18:01:29 -0400

    finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,27 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...removes) {
+    let newArray = [];
+    
+    for (let i = 0; i < array.length; i++) {
+        let check = true;
+
+        for (let remove of removes) {
+            if (array[i] === remove) {
+                check = false;
+                break;
+            }
+
+
+
+        }
+
+        if (check == true) {
+            newArray.push(array[i]);
+        }
+
+        
+
+    }
+    return newArray;
 };
 
 // Do not edit below this line
