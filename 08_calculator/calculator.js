b1e35f6d0548668c26854d910a5f793a949d7e62
Nikita <ovramenko.nikitka@gmail.com>
2024-07-24 21:08:56 -0400

    finished 8,9,10

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,53 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let finalSum = array.reduce((prev,next) => {
+    return prev + next;
+  },0)
+
+  return finalSum;
+
 };
 
-const multiply = function() {
+const multiply = function(array) {
+
+  let finalMultiply = array.reduce((prev,next) => {
+    return prev * next;
+  },1);
+
+  return finalMultiply;
 
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+
+    let ans = 1;
+    for(let i = 0; i < y; i++){
+      ans *= x;
+    }
+
+    return ans;
 };
 
-const factorial = function() {
+const factorial = function(number) {
+
+  if(number === 0) {
+    return 1;
+  }
+
+  let final = 1;
+  for(let i = number; i >= 1; i--){
+    
+    final *= i;
+  }
+
+  return final;
 	
 };
 
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
 
+
+    let clearSen = sentence.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g, '');
+    clearSen = clearSen.toLowerCase();
+    
+
+
+
+    let reverseSen = "";
+   for(let i = clearSen.length - 1 ; i >= 0; i--){
+
+    
+    reverseSen += clearSen[i];
+   }
+
+   console.log(reverseSen);
+
+   if(reverseSen == clearSen){
+    return true;
+   }
+
+   else {
+    return false;
+   }
 };
 
 // Do not edit below this line
